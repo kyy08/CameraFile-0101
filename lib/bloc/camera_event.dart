1cd9a3c503c9c5cb4b12e9b6f68b82e0cedaaf72
kyy08 <kyyelzz1@gmail.com>
2025-05-16 00:45:56 +0700

    membuat event ambil gambar dan tap-to-focus

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -9,3 +9,14 @@ final class SwitchCamera extends CameraEvent {}
 
 final class ToogleFlash extends CameraEvent {}
 
+final class TakePicture extends CameraEvent {
+  final void Function(File ImageFile) onPictureTaken;
+  TakePicture(this.onPictureTaken);
+}
+
+final class TapToFocus extends CameraEvent {
+  final Offset position;
+  final Size previewSize;
+  TapToFocus(this.position, this.previewSize);
+}
+
