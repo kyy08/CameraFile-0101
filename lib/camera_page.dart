623519a9e4c575597fb1a46192f0fa0dfa4142c8
kyy08 <kyyelzz1@gmail.com>
2025-05-16 00:29:46 +0700

    membuat tampilan pratinjau kamera dengan tombol switch, flash, dan ambil gambar

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -31,4 +31,80 @@ class _CameraPageBcState extends State<CameraPageBc> {
     };
   }
 
-  
+  Widget _circleButton(IconData icon, VoidCallback onTap) {
+    return ClipOval(
+      child: Material(
+        color: Colors.white24,
+        child: InkWell(
+          onTap: onTap,
+          child: SizedBox(
+            width: 50,
+            height: 50,
+            child: Icon(icon, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: BlocBuilder<CameraBloc, CameraState>(
+        builder: (context, state) {
+          if (state is! CameraReady) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              return Stack(
+                fit: StackFit.expand,
+                children: [
+                  GestureDetector(
+                    onTapDown: (details) {
+                      context.read<CameraBloc>().add(
+                        TapToFocus(details.localPosition, constraints.biggest),
+                      );
+                    },
+                    child: CameraPreview(state.controller),
+                  ),
+                  Positioned(
+                    top: 50,
+                    right: 20,
+                    child: Column(
+                      children: [
+                        _circleButton(Icons.flip_camera_android, () {
+                          context.read<CameraBloc>().add(SwitchCamera());
+                        }),
+                        const SizedBox(height: 12),
+                        _circleButton(_flashIcon(state.flashMode), () {
+                          context.read<CameraBloc>().add(ToogleFlash());
+                        }),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 40,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: FloatingActionButton(
+                        backgroundColor: Colors.white,
+                        onPressed: () {
+                          context.read<CameraBloc>().add(
+                            TakePicture((file) => Navigator.pop(context, file)),
+                          );
+                        },
+                        child: const Icon(Icons.camera_alt, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
