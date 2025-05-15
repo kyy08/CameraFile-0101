483998c421a20846af2dde1347245ada6c990e17
kyy08 <kyyelzz1@gmail.com>
2025-05-16 00:13:30 +0700

    membuat HomePage

---
+++ lib/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:camera_app/bloc/camera_bloc.dart';
+import 'package:camera_app/bloc/camera_event.dart';
+import 'package:camera_app/bloc/camera_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  
