3f442b3a6cb75bd8984218a99eb92f00e553bb7a
kyy08 <kyyelzz1@gmail.com>
2025-05-16 00:17:37 +0700

    membuat struktur tampilan dasar  HomePage dengan tombol kamera dan galeri

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,4 +7,47 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Beranda')),
+      body: SafeArea(
+        child: BlocConsumer<CameraBloc, CameraState>(
+          listener: (context, state) {
+            if (state is CameraReady && state.snackBarMessage != null) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(state.snackBarMessage!)));
+              context.read<CameraBloc>().add(ClearSnackbar());
+            }
+          },
+           builder: (context, state) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton.icon(
+                        icon: const Icon(Icons.camera),
+                        label: const Text('Ambil Foto'),
+                        onPressed: () {
+                          final bloc = context.read<CameraBloc>();
+                          if (bloc.state is! CameraReady) {
+                            bloc.add(InitializeCamera());
+                          }
+                          bloc.add(OpenCameraAndCapture(context));
+                        },
+                      ),
+                    ),
+                    ElevatedButton.icon(
+                      icon: Icon (Icons.folder),
+                      label: const Text('Pilih dari Galeri'),
+                      onPressed: () => context
+                          .read<CameraBloc>()
+                          .add(PickImageFromGallery()),
+                    ), 
+                  ],
+                ),
+                const SizedBox(height: 20),
+                
