0a570e41cc38a744e39d644b6de32cfcf8cca8b1
kyy08 <kyyelzz1@gmail.com>
2025-05-16 00:35:04 +0700

    menambah CameraReady dengan controller, index, flashMode, dan file gambar opsional

--- lib/bloc/camera_state.dart
+++ lib/bloc/camera_state.dart
@@ -5,3 +5,18 @@ sealed class CameraState {}
 
 final class CameraInitial extends CameraState {}
 
+final class CameraReady extends CameraState {
+  final CameraController controller;
+  final int selectedIndex;
+  final FlashMode flashMode;
+  final File? imageFile;
+  final String? snackBarMessage;
+
+  CameraReady({
+    required this.controller,
+    required this.selectedIndex,
+    required this.flashMode,
+      this.imageFile,
+    this.snackBarMessage,
+  });
+  
